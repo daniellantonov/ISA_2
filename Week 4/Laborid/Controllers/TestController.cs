7ef2aaad0bc573fbdcb96c254ca0d8c821f37399
daniellantonov <daniellantonov@gmail.com>
2019-02-25 16:53:38 +0200

    Labor 1-3

---
+++ Week 4/Laborid/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.NET_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Laborid.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello World!";
+        }
+        // GET: /<controller>/
+        public IActionResult GetView()
+        {
+            Employee emp = new Employee
+            {
+                FirstName = "Daniel",
+                LastName = "Antonov",
+                Salary = 50000
+            };
+            ViewData["Employee"] = emp;
+            return View("MyView");
+        }
+    }
+}
