3804b888f8fb62bcebf812e64a53c4fd7b7010b0
daniellantonov <daniellantonov@gmail.com>
2019-02-25 14:21:32 +0200

    test

---
+++ Week 1/ClassLibrary/Libraries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace ClassLibrary
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+
+            Console.WriteLine(reply);
+            try
+            {
+                File.WriteAllText("WriteFile.txt", reply);
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
