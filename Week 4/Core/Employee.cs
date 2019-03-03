33c54d5e3eb79d3ebfce7d217e3206ac3bbfef51
Daniel Antonov <daniellantonov@gmail.com>
2019-03-03 21:20:42 +0200

    Labor 14

--- Week 4/Core/Employee.cs
+++ Week 4/Core/Employee.cs
@@ -8,24 +8,21 @@ namespace Core
 {
     public class Employee
     {
-        private const string requiredField = "Required field!";
-        private const string lengthIsTooBig = "Length should be less than 20 characters!";
         public Employee()
         {
         }
 
-        public Employee(string firstName, string lastname=null, int salary=0)
+        public Employee(string firstName, string lastname = null, int salary = 0)
         {
-            FirstName = firstName?? string.Empty;
-            LastName = lastname?? string.Empty;
+            FirstName = firstName ?? string.Empty;
+            LastName = lastname ?? string.Empty;
             Salary = salary;
         }
 
         public int EmployeeId { get; set; }
-        [Required(ErrorMessage = requiredField)]
-        [StringLength(20, ErrorMessage = lengthIsTooBig)]
+        [NameValidation]
         public string FirstName { get; set; }
-        [StringLength(20, ErrorMessage = lengthIsTooBig)]
+        [NameValidation]
         public string LastName { get; set; }
         public int Salary { get; set; }
     }
