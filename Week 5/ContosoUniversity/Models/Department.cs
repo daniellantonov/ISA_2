f0079fbc943969e0f6659a3e5c3cfec66f0b827d
daniellantonov <daniellantonov@gmail.com>
2019-03-18 16:17:30 +0200

    Razor pages 2.1

--- Week 5/ContosoUniversity/Models/Department.cs
+++ Week 5/ContosoUniversity/Models/Department.cs
@@ -13,8 +13,7 @@ namespace ContosoUniversity.Models
         public string Name { get; set; }
 
         [DataType(DataType.Currency)]
-
-       [Column(TypeName = "money")]
+        [Column(TypeName = "money")]
         public decimal Budget { get; set; }
 
         [DataType(DataType.Date)]
@@ -23,6 +22,9 @@ namespace ContosoUniversity.Models
         public DateTime StartDate { get; set; }
 
         public int? InstructorID { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
 
         public Instructor Administrator { get; set; }
         public ICollection<Course> Courses { get; set; }
---
+++ Week 7/ContosoUnivesity/Pages/Students/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUnivesity.Models;
+
+namespace ContosoUnivesity.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ContosoUnivesity.Models.SchoolContext _context;
+
+        public DetailsModel(ContosoUnivesity.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Student.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
